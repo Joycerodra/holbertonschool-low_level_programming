42ea62411b1096c32f7498ce5dcc72e916c23ee1
Joycerodra <asistenteph2012@gmail.com>
2021-10-04 21:51:22 -0500

    4puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ *main - Is where the program start its execution.
+ *
+ * Return:  always 0
+ */
+int main(void)
+{
+  puts("\"Programming is like building a multilingual puzzle");
+
+  return (0);
+}
