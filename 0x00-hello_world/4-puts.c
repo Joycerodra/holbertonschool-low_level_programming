9ea2f84af3e9426ab38824afad84c25165f1625f
Joycerodra <asistenteph2012@gmail.com>
2021-10-04 22:01:26 -0500

    4puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main - where program Init the x.
- *
- * Return:  always 0
- */
+*main - where program Init the x.
+*
+* Return:  always 0
+*/
 int main(void)
 {
   puts("\"Programming is like building a multilingual puzzle");
