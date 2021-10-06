3ee0bf722120e1f68a6b308487c79ea2a6839d76
Joycerodra <asistenteph2012@gmail.com>
2021-10-06 18:11:09 -0500

    main

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_HEADER
+#define MAIN_HEADER
+/*Incluir librerias*/
+#include <unistd.h>
+#include <stdio.h>
+/*incluir prototipo de funcion ejemplo:*/
+int _isalpha(int c);
+#endif /*MAIN_HEADER*/  /* Final del archivo cabecera */
