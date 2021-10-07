ef5bacfb5a1dc5b9579b6913175f49fb6ee17552
Joycerodra <asistenteph2012@gmail.com>
2021-10-06 19:45:57 -0500

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include <main.h>
+#include "main.h"
 
 /**
  *main - Write a program that prints _putchar
@@ -6,18 +6,17 @@
  *Return: 0
  */
 
-int main (void)
+int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
-  _putchar('P');
-  _putchar('u');
-  _putchar('t');
-  _putchar('c');
-  _putchar('h');
-  _putchar('a');
-  _putchar('r');
-  _putchar('\n');
-
-  return (0);
-
+	return (0);
 }
