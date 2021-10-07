3bdb93cb06ea56ee919bfc7a88e1fcb067fe530f
Joycerodra <asistenteph2012@gmail.com>
2021-10-07 12:22:15 -0500

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,10 +2,10 @@
 
 /**
  * main - program to print alphabet from a to z
+ * print_alphabet - prints the all alphabet from a to z one by one
  *
  * Return: Always 0.
  */
-
 void print_alphabet(void)
 {
 	char i;
