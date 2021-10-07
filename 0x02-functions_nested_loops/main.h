da810bde9518e7855c89c020123256518bcf99f6
Joycerodra <asistenteph2012@gmail.com>
2021-10-06 19:50:49 -0500

    putchar

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,8 +1,12 @@
-#ifndef MAIN_HEADER
-#define MAIN_HEADER
+#ifndef MAIN_H
+#define MAIN_H
+
 /*Incluir librerias*/
+
 #include <unistd.h>
 #include <stdio.h>
+
 /*incluir prototipo de funcion ejemplo:*/
+int _putchar(char c);
 int _isalpha(int c);
-#endif /*MAIN_HEADER*/  /* Final del archivo cabecera */
+#endif /*MAIN_H*/  /* Final del archivo cabecera */
