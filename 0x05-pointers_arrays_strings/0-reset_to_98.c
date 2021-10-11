3880fcf100c1e88b08f6f8ee70bd40d4552bc469
Joycerodra <asistenteph2012@gmail.com>
2021-10-11 11:53:19 -0500

    98

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - main function
+ *@n: point with the adress value
+ *
+ * Description: This function take a pointer to an int as parameter
+ *
+ *
+ */
+void reset_to_98(int *n)
+{
+	*n = 98
+}
