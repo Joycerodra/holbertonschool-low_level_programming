26ac7f5c066128031dd0bc8d4447a6d64a75c0f8
Joycerodra <asistenteph2012@gmail.com>
2021-10-22 04:07:03 -0500

    nested

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/*Incluir librerias*/
-
-#include <unistd.h>
-#include <stdio.h>
-
-/*incluir prototipo de funcion ejemplo:*/
 int _putchar(char c);
 int _isalpha(int c);
 void print_alphabet(void);
-#endif /*MAIN_H*/  /* Final del archivo cabecera */
+
+#endif 
