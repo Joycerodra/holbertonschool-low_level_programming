6a33304689069178a8ef492c956b832bf369a77a
Joycerodra <asistenteph2012@gmail.com>
2022-02-12 02:44:03 -0500

    alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int c;
+
+	do {
+		for (c = 'a'; c <= 'z'; c++)
+			putchar(c);
+	} while (c < 'z');
+	do {
+		for (c = 'A'; c <= 'Z'; c++)
+			putchar(c);
+	} while (c < 'Z');
+	putchar('\n');
+	return (0);
+}
