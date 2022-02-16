2fd026e3badff176f5837efeb9b2d0a8dd1983ae
Joycerodra <asistenteph2012@gmail.com>
2022-02-16 00:09:59 -0500

    98

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *print_to_98 - prints all number form integer to 98
+ *@a: integer
+ */
+
+void print_to_98(int a)
+{
+	while (a != 98)
+	{
+		printf("%d, ", a);
+		if (a > 98)
+		{
+			a--;
+		}
+		else if (a < 98)
+		{
+			a++;
+		}
+	}
+	printf("%d\n", 98);
+}
